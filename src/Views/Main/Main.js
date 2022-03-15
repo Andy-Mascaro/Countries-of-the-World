25291b7fbe217a89870baaa948ac3832bb3e6a35
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-15 14:21:12 -0700

    added to my default function in main

--- src/Views/Main/Main.js
+++ src/Views/Main/Main.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import 
+import { useEffect, useState} from 'react/cjs/react.production.min';
+import './Main.css';
 
 export default function Main() {
   return (
-    <div>Main</div>
+    
+    const [country, setCountry] = useState([]);
+
+    useEffect(() => {
+        const fetch = async () => {
+        const resp = await fetchCountries();
+        setCountries(resp);
+        };
+        fetch();
+    }, []);
+    // return (
+    //     <div className="main">main
+    //     {countries.map((item) => (
+    //      <CountryCard  key={item.id} 
+    //     ))}
+    //     </div>
+    )
+    })
+
+    
   );
 }
